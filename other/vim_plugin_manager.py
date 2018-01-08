8aea9f1a7f0d112b34b4026787f1c46f45badb49
ritarock <dvryz1204@gmail.com>
2018-01-08 23:29:43 +0900

    Create vim_plugin_manager.py

---
+++ other/vim_plugin_manager.py
@@ -0,0 +1,21 @@
+import subprocess
+
+def plug():
+    pluginList = [
+    'tomasr/molokai',
+    'Townk/vim-autoclose'
+    ]
+    print(pluginList)
+    addPlugin(pluginList)
+
+def addPlugin(pluginList):
+    for plugin in pluginList:
+        # print(plugin)
+        gitUrl = "https://github.com/"
+        # print("git clone " + gitUrl + plugin + ".git" + " ~/.vim/pack/mypackage/start/")
+        repo = gitUrl + plugin +'.git'
+        subprocess.run(['git', 'clone', repo, ' ~/.vim/pack/mypackage/start/'],shell=True)
+
+
+if __name__ == '__main__':
+    plug()
