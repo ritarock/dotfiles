b460fa09fc21a241ce4f258ad7bb808c0b109f8d
ritarock <dvryz1204@gmail.com>
2018-01-10 21:51:02 +0900

    Update install_python.sh

--- other/shell/install_python.sh
+++ other/shell/install_python.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sudo apt-get update
-sudo apt-get install git 
+sudo apt-get install -y git
 sudo apt-get install build-essential libncursesw5-dev libgdbm-dev libc6-dev zlib1g-dev libsqlite3-dev tk-dev libssl-dev openssl libbz2-dev libreadline-dev
 git clone https://github.com/yyuu/pyenv.git ~/.pyenv
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.profile
