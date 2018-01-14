0488749cea8eba0690cc5d5f599f9555b3daaeaf
ritarock <dvryz1204@gmail.com>
2018-01-14 15:36:01 +0900

    Update install_go.sh

--- other/shell/install_go.sh
+++ other/shell/install_go.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 sudo apt-get update
-sudo apt-get -y upgrade
-wget https://storage.googleapis.com/golang/go1.9.2.linux-amd64.tar.gz
-sudo tar -xvf go1.9.2.linux-amd64.tar.gz
-sudo mv go /usr/local
-export GOROOT=/usr/local/go
-export GOPATH=$HOME/Projects/Proj1
-export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
-go version
-go env
+sudo apt-get -y install git
+sudo git clone https://github.com/wfarr/goenv.git ~/.goenv
+# ~/.bashrcに追記
+export PATH="$HOME/.goenv/bin:$PATH"
+eval "$(goenv init -)"
+source ~/.bashrc
+goenv install 1.9.2
+goenv global 1.9.2
