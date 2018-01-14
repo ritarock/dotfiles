5519d19b29070f324edc2c633781b909d8eb9c9d
ritarock <dvryz1204@gmail.com>
2018-01-14 16:19:30 +0900

    Update install_go.sh

--- other/shell/install_go.sh
+++ other/shell/install_go.sh
@@ -2,9 +2,9 @@
 sudo apt-get update
 sudo apt-get -y install git
 sudo git clone https://github.com/wfarr/goenv.git ~/.goenv
-# ~/.bashrcに追記
-export PATH="$HOME/.goenv/bin:$PATH"
-eval "$(goenv init -)"
+echo 'export PATH="$HOME/.goenv/bin:$PATH"' >> ~/.bashrc
+'eval "$(goenv init -)"' >> ~/.bashrc
 source ~/.bashrc
 goenv install 1.9.2
 goenv global 1.9.2
+
