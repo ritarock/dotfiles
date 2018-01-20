5765ca1a0e1d0d07c5f9f1d29ba4997ff91921b2
ritarock <dvryz1204@gmail.com>
2018-01-21 01:27:19 +0900

    Update vim_plugin_manager.py

--- other/test_python/vim_plugin_manager.py
+++ other/test_python/vim_plugin_manager.py
@@ -1,22 +1,23 @@
-import subprocess
 import os
+import subprocess
+import json
 
 def plug():
-    pluginList = [
-    'tomasr/molokai',
-    'Townk/vim-autoclose'
-    ]
-    print(pluginList)
-    addPlugin(pluginList)
+    f = open('plugin_list.json','r')
+    jsonData = json.load(f)
+    #print(jsonData)
+    addPlugin(jsonData)
 
 def addPlugin(pluginList):
     for plugin in pluginList:
-        # print(plugin)
         gitUrl = "https://github.com/"
-        # print("git clone " + gitUrl + plugin + ".git" + " ~/.vim/pack/mypackage/start/")
+        #print("git clone " + gitUrl + plugin + ".git" + " ~/.vim/pack/mypackage/start/")
         repo = gitUrl + plugin +'.git'
-        subprocess.run(['git', 'clone', repo, ' ~/.vim/pack/mypackage/start/'],shell=True)
+        homePath = os.path.expanduser('~')
+        path = " " + homePath + "/.vim/pack/mypackage/start/"
+        # print ('git clone' + repo + path)
 
+        subprocess.run(['git','clone',repo,path])
 
 if __name__ == '__main__':
     plug()
