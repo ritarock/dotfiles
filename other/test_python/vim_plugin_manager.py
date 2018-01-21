c2783385af8322ffc5cdaf87edca1dcdc47c2a2a
ritarock <dvryz1204@gmail.com>
2018-01-21 16:13:54 +0900

    Update vim_plugin_manager.py

--- other/test_python/vim_plugin_manager.py
+++ other/test_python/vim_plugin_manager.py
@@ -5,18 +5,14 @@ import json
 def plug():
     f = open('plugin_list.json','r')
     jsonData = json.load(f)
-    #print(jsonData)
     addPlugin(jsonData)
 
 def addPlugin(pluginList):
     for plugin in pluginList:
         gitUrl = "https://github.com/"
-        #print("git clone " + gitUrl + plugin + ".git" + " ~/.vim/pack/mypackage/start/")
         repo = gitUrl + plugin +'.git'
         homePath = os.path.expanduser('~')
-        path = " " + homePath + "/.vim/pack/mypackage/start/"
-        # print ('git clone' + repo + path)
-
+        path = homePath + "/.vim/pack/mypackage/start/" + plugin.split("/")[1]
         subprocess.run(['git','clone',repo,path])
 
 if __name__ == '__main__':
