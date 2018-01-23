c38ef3f88835869a16fa48b4421a46db2285f6d9
ritarock <dvryz1204@gmail.com>
2018-01-23 23:27:35 +0900

    Update vim_plugin_manager.py

--- other/test_python/vim_plugin_manager.py
+++ other/test_python/vim_plugin_manager.py
@@ -11,30 +11,33 @@ def main():
     now_plugin = os.listdir(path)
     add_list = []
     remove_list = []
-    # print(now_plugin)
-    for plugin in plugin_list:
-        check_plugin.append(plugin.split('/'))
-        # print(check_plugin)
+    # print(len(now_plugin))
+    if len(now_plugin) == 0:
+        for plugin in plugin_list:
+            add_list.append(plugin.split('/'))
+            addfunc(add_list,home_path)
+    else:
+        for plugin in plugin_list:
+            check_plugin.append(plugin.split('/'))
+            # print(check_plugin)
 
-    for now in now_plugin:
-        for add in check_plugin:
-            if add[1] == now:
-                pass
-            else:
-                add_list.append(add)
-    # print(add)
+            for now in now_plugin:
+                for add in check_plugin:
+                    if add[1] == now:
+                        pass
+                    else:
+                        add_list.append(add)
 
     addfunc(add_list,home_path)
 
 def addfunc(add_list,home_path):
     for plugin in add_list:
         git_url = 'https://github.com/'
-        print ('/'.join(plugin))
+        # print ('/'.join(plugin))
         repo = git_url + '/'.join(plugin) + '.git'
-        path = home_path + '/.vim/pack/mypackage/start/' + '/'.join(plugin) 
-        #print ('git clone' + repo + path)
+        path = home_path + '/.vim/pack/mypackage/start/'
+        # print ('git clone' + repo + path)
         subprocess.run(['git','clone',repo,path])
-
 
 if __name__ == '__main__':
     main()
