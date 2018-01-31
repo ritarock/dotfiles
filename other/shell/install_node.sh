163aed30a79405e61f0bf166bb959687c6c4ed43
ritarock <dvryz1204@gmail.com>
2018-01-31 21:28:47 +0900

    Create install_node.sh

---
+++ other/shell/install_node.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+sudo apt-get update
+sudo apt-get install -y build-essential libssl-dev
+sudo apt-get install -y curl
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
+source ~/.bashrc
+nvm install v7.10
+node -v
+nvm ls-remote
+nvm install v8.9.4
