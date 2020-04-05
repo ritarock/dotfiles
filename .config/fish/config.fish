5358e626a382a7560ba1c4a6da07061106d1fb24
ritarock <dvryz1204@gmail.com>
2020-04-05 16:40:31 +0900

    update

---
+++ .config/fish/config.fish
@@ -0,0 +1,25 @@
+. ~/.config/fish/aliases.fish
+. ~/.config/fish/env.fish
+
+fish_vi_key_bindings
+
+function cd
+  builtin cd $argv
+  ls
+end
+
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showstashstate 'yes'
+set __fish_git_prompt_showuntrackedfiles 'yes'
+set __fish_git_prompt_showupstream 'yes'
+set __fish_git_prompt_color_branch yellow
+set __fish_git_prompt_color_upstream_ahead green
+set __fish_git_prompt_color_upstream_behind red
+
+# Status Chars
+set __fish_git_prompt_char_dirtystate 'x'
+set __fish_git_prompt_char_stagedstate '↑'
+set __fish_git_prompt_char_untrackedfiles '☡'
+set __fish_git_prompt_char_stashstate '↩'
+set __fish_git_prompt_char_upstream_ahead '+'
+set __fish_git_prompt_char_upstream_behind '-'
