2753a05d7526afcb9d4da37b8d619ec98432bfb0
ritarock <dvryz1204@gmail.com>
2020-10-28 13:55:39 +0900

    update

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -1,13 +1,17 @@
 alias ltr 'ls -ltr'
 alias tree 'tree -NC'
+
 alias gst 'git status'
 alias gdf 'git diff --color-words'
 alias gcm 'git checkout master'
-alias gc 'git checkout -'
+alias gc 'git branch -a | fzf | xargs git checkout'
 alias gpu 'git pull upstream master'
+alias gpr 'git push --set-upstream origin (git name-rev --name-only HEAD)'
 alias glog 'git log --oneline'
+alias grh 'git reset --mixed HEAD^'
+
 alias dcu 'docker-compose up'
 alias dcd 'docker-compose down'
 alias dcr 'docker-compose run --rm'
-alias dpa 'docker ps -a'
 alias dpurge 'docker stop (docker ps -aq) && docker rm (docker ps -aq)'
+
