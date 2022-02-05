d5f53837227def5430984f917fcc961c64dae926
ritarock <dvryz1204@gmail.com>
2022-02-05 16:34:44 +0900

    update

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -4,17 +4,14 @@ alias tree 'tree -NC'
 alias gst 'git status'
 alias gdf 'git diff --color-words'
 alias gcm 'git checkout master'
-alias gc 'git branch -a | fzf | xargs git checkout'
 alias gpu 'git pull upstream master'
 alias gpr 'git push --set-upstream origin (git name-rev --name-only HEAD)'
 alias glog 'git log --oneline'
 alias grh 'git reset --mixed HEAD^'
+alias gbdel 'git branch --merged | grep -v "*" | xargs -I git branch -d %'
 
 alias dcu 'docker-compose up'
 alias dcd 'docker-compose down'
 alias dcr 'docker-compose run --rm'
-alias dpa 'docker ps -a'
 alias dpurge 'docker stop (docker ps -aq) && docker rm (docker ps -aq)'
-
-alias gbdel 'git branch -r --merged origin/master | grep -v -e master  | sed -e \'s/origin\///g\' | xargs -I{} git push -d origin {}'
 
