cc9fd2574790bad6da01fee5504fd0744aabbdf0
ritarock <dvryz1204@gmail.com>
2023-07-23 01:47:01 +0900

    update

--- roles/configs/files/fish/aliases.fish
+++ roles/configs/files/fish/aliases.fish
@@ -4,12 +4,12 @@ alias tree 'tree -NC'
 alias .. 'cd ./..'
 
 alias gst   'git status'
-alias gd    'git diff --color-words'
-alias gp    'git pull'
+alias gdiff 'git diff --color-words'
+alias gpull 'git pull'
 alias gb    'git branch'
-alias gbc   'git checkout -b'
-alias gco   'git branch | fzf | xargs git checkout'
-alias gl    'git log --oneline'
+alias gco   'git checkout'
+alias glog  'git log --oneline'
+alias glogg 'git log --graph'
 alias gbdel 'git branch --merged | grep -v "*" | xargs -I git branch -d %'
 
 alias dcu    'docker-compose up'
@@ -18,3 +18,4 @@ alias dcr    'docker-compose run --rm'
 alias dpurge 'docker stop (docker ps -aq) && docker rm (docker ps -aq)'
 
 alias gmt 'go mod tidy'
+
