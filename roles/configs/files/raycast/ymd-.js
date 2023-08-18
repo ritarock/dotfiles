02d01157cc5991aee8b21e942fb966de307d8e2b
ritarock <dvryz1204@gmail.com>
2023-08-19 02:02:57 +0900

    update

---
+++ roles/configs/files/raycast/ymd-.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+// Required parameters:
+// @raycast.schemaVersion 1
+// @raycast.title ymd-
+// @raycast.mode compact
+
+// Optional parameters:
+// @raycast.icon 🤖
+
+const ymd_ = () => {
+  const dt = new Date()
+  const yyyy = dt.getFullYear()
+  const mm = ("0" + (dt.getMonth() + 1)).slice(-2)
+  const dd = ("0" + dt.getDate()).slice(-2)
+
+  return `${yyyy}-${mm}-${dd}`
+}
+
+console.log(ymd_())
