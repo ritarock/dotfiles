eb758d40bfad0e48efd374e729ef48d79b94bcb1
ritarock <dvryz1204@gmail.com>
2023-11-03 13:18:00 +0900

    update

--- roles/configs/files/fish/aliases.fish
+++ roles/configs/files/fish/aliases.fish
@@ -3,9 +3,6 @@ alias tree 'tree -NC'
 
 alias .. 'cd ./..'
 
-alias dcup   'docker-compose up'
-alias dcdown 'docker-compose down'
-alias dcr    'docker-compose run --rm'
 alias dpurge 'docker stop (docker ps -aq) && docker rm (docker ps -aq)'
 
 alias gmt 'go mod tidy'
