9a3bc9efabe8b46faa7701cbd4dca3010874167f
ritarock <dvryz1204@gmail.com>
2023-12-29 15:58:37 +0900

    update

--- roles/configs/files/fish/git_aliases.fish
+++ roles/configs/files/fish/git_aliases.fish
@@ -1,20 +1,26 @@
-alias gb    'git branch'
+alias gb 'git branch'
 alias gbdel 'git branch --merged | grep -v "*" | xargs -I git branch -d %'
 
-alias gco   'git checkout'
-alias gsw   'git switch'
+alias gco 'git checkout'
+alias gsw 'git switch'
 
 alias gdiff 'git diff --color-words'
 
-alias glog  'git log --oneline'
+alias glog 'git log --oneline'
 alias glogg 'git log --graph'
 
 alias gpull 'git pull'
 
-alias gst   'git status'
+alias gst 'git status'
 
-alias gss   'git stash save'
-alias gsl   'git stash list'
-alias gsp   'git stash pop stash@{0}'
-alias gsd   'git stash drop stash@{0}'
+alias gss 'git stash save -u'
+alias gsl 'git stash list'
+
+function gspop
+  git stash pop "stash@{$argv[1]}"
+end
+
+function gsdrop
+  git stash drop "stash@{$argv[1]}"
+end
 
