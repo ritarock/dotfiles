8783e9fdc255ed48c5807ecf44f96914f621c33e
ritarock <dvryz1204@gmail.com>
2025-01-13 17:55:35 +0900

    update

--- roles/configs/files/zsh/aliases.zsh
+++ roles/configs/files/zsh/aliases.zsh
@@ -1,12 +1,15 @@
-alias ltr='ls -ltr'
-alias ll='ls -l'
+alias ltr='eza -lr'
+alias ll='eza -l'
+alias ls='eza'
 
 alias tree='tree -NC'
 
 alias ..='cd ./..'
+alias ~='cd ~'
 
 alias gmt='go mod tidy'
 
 alias k='kubectl'
 
 alias v='vim'
+alias c='code'
