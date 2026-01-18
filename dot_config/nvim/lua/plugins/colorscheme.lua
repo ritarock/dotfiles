1a4d4735359fd161bab82c4b7f2fb76e4cbf5bc4
ritarock <dvryz1204@gmail.com>
2026-01-18 17:26:16 +0900

    update

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,25 +1,15 @@
 return {
   {
-    "ritarock/shinonome",
-    config = function()
-      vim.api.nvim_create_autocmd("ColorScheme", {
-        pattern = "shinonome",
-        callback = function()
-          local fg = "#c8ccd4"
-          -- Snacks picker
-          vim.api.nvim_set_hl(0, "SnacksPickerPathHidden", { fg = fg })
-          vim.api.nvim_set_hl(0, "SnacksPickerPath", { fg = fg })
-          vim.api.nvim_set_hl(0, "SnacksPickerFile", { fg = fg })
-          vim.api.nvim_set_hl(0, "SnacksPickerDir", { fg = "#61afef" })
-          vim.api.nvim_set_hl(0, "NonText", { fg = "#6a6a6a" })
-        end,
-      })
-    end,
+    "maxmx03/solarized.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
   },
+
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "shinonome",
+      colorscheme = "solarized",
     },
   },
 }
